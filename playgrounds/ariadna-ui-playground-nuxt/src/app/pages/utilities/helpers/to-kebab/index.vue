fbb20a0c2e6d1a85d39fc50074deefa522d7b1f6
iWatchYouFromAfar <skip1209@yahoo.com>
2025-04-04 15:27:35 +0300

    refactor: rename CamelotToKebab import to ToKebab

--- playgrounds/ariadna-ui-playground-nuxt/src/app/pages/utilities/helpers/to-kebab/index.vue
+++ playgrounds/ariadna-ui-playground-nuxt/src/app/pages/utilities/helpers/to-kebab/index.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script setup lang="ts">
-import toKebab from '@iwyfaf-vue-ui/ariadna-ui/CamelToKebab';
+import toKebab from '@iwyfaf-vue-ui/ariadna-ui/ToKebab';
 
 definePageMeta({
   layout: 'default',
