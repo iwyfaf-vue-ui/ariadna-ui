440875af237f9563867a587990a0be4b71489d64
iWatchYouFromAfar <skip1209@yahoo.com>
2025-04-13 21:25:33 +0300

    playground: add Badge playground

--- playgrounds/ariadna-ui-playground-nuxt/pages/components/indicators/badge/index.vue
+++ playgrounds/ariadna-ui-playground-nuxt/pages/components/indicators/badge/index.vue
@@ -1,26 +1,32 @@
 <template>
   <h1>Button</h1>
 
-  <Badge size="small">0</Badge>
-  <Badge size="small" modifier="primary">2</Badge>
-  <Badge size="small" modifier="secondary">4</Badge>
-  <Badge size="medium" modifier="success">6</Badge>
-  <Badge size="medium" modifier="warning">8</Badge>
-  <Badge size="large" modifier="danger">10</Badge>
+  <Badge size="small">docs v1.0.0</Badge>
+  <Badge size="small" modifier="primary">app v1.12.432</Badge>
 
-  <Button size="large" modifier="secondary">
+  <hr />
+
+  <Badge size="small" modifier="primary" rounded>2</Badge>
+  <Badge size="small" modifier="secondary" rounded>4</Badge>
+  <Badge size="medium" modifier="success" rounded>6</Badge>
+  <Badge size="medium" modifier="warning" rounded>8</Badge>
+  <Badge size="large" modifier="danger" rounded>10</Badge>
+
+  <hr />
+
+  <Button size="large" modifier="primary">
     Default Badge
     <Badge size="small">2</Badge>
   </Button>
 
   <Button size="large" modifier="secondary">
     Primary Badge
-    <Badge size="small" modifier="primary">2</Badge>
+    <Badge size="small" modifier="primary" rounded>2</Badge>
   </Button>
 
   <Button size="large" modifier="success">
     Floating Badge
-    <Badge size="small" modifier="success" floating>2</Badge>
+    <Badge size="small" modifier="success" floating rounded>2</Badge>
   </Button>
 </template>
 
